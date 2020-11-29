46ed49b4a4482f6202ccb3cf31ad2b43c52da021
rakaadi <56627679+rakaadi@users.noreply.github.com>
2020-11-29 21:28:06 +0700

    Add GlobalStyles import from twin.macro

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import { AppProps } from "next/app"
+import { GlobalStyles } from "twin.macro"
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <GlobalStyles />
       <Component {...pageProps} />
     </>
   )
